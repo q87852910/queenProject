50268b7c1eab8356b722b56323b0df726e769212
libinyu <libinyu>
2021-03-16 15:57:36 +0800

    添加生成者demo

---
+++ kafkaProducer/src/main/java/com/kafka/test/MainClass.java
@@ -0,0 +1,11 @@
+package com.kafka.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        List a =new ArrayList<>();
+    }
+}
--- kafkaProducer/src/main/java/com/pojo/User.java
+++ kafkaProducer/src/main/java/com/pojo/User.java
@@ -4,10 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-    private String id;
-    private String name;
+    private String[] id;
+    private List<String> name;
+
+
 }
